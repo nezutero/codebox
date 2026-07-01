0554d57670bbccf7e866750b3aed6469ff7c7630
nezutero <me@nezutero.dev>
2026-07-01 17:40:51 +0200

    chore: update codeberg

--- bc_course/weight_converter.c
+++ bc_course/weight_converter.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 int main() {
     int choice = 0;
@@ -7,19 +7,20 @@ int main() {
     float pounds = 0.0;
 
     printf("--- Weight Converter ---\n");
-    printf("Options (1 or 2):\n(1) Kilograms -> Pounds\n(2) Pounds -> Kilograms\n ");
-    scanf("%d",&choice);
-    
+    printf(
+            "Options (1 or 2):\n(1) Kilograms -> Pounds\n(2) Pounds -> Kilograms\n ");
+    scanf("%d", &choice);
+
     if (choice == 1) {
         printf("Enter the # to convert (kg -> lb): ");
-        scanf("%f",&kilograms);
+        scanf("%f", &kilograms);
 
         pounds = kilograms * 2.2046;
         printf("%.2f kg = %.2f lb\n", kilograms, pounds);
 
     } else if (choice == 2) {
         printf("Enter the # to convert (lb -> kg): ");
-        scanf("%f",&pounds);
+        scanf("%f", &pounds);
 
         kilograms = pounds / 2.2046;
         printf("%.2f lb = %.2f kg\n", pounds, kilograms);
@@ -28,6 +29,7 @@ int main() {
         printf("Wrong input\n");
         return 1;
     }
-    
+
+    printf(fn(3, 4));
     return 0;
 }
